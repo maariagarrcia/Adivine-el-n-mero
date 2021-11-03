b1b5c340c522b8a9b62c64cfb2a77ddd6227a2f8
maariagarrcia <mrrmn@myuax.com>
2021-11-03 17:04:20 +0100

    guardar

--- juego.py
+++ juego.py
@@ -2,21 +2,22 @@ import random
 numero_aleatorio=random.randint(0,100)
 numero_aleatorio= int
 print (numero_aleatorio)
+intento=0
+intentos= intento+1
 
-print("piensa un número entre" + str(min) + str(max))
+while True:
+    if 0<=numero_aleatorio<=99:
+        print("Numero entre los valores 0 y 99")
 
 numero_usuario= input()
 
-while numero_aleatorio==numero_usuario:
-    if numero_aleatorio>numero_usuario:
-        print("Demasiado grande")
+intentos
+    while numero_aleatorio==numero_usuario:
+        if numero_aleatorio>numero_usuario:
+            print("Demasiado grande")
 
-    else:
-        print("Demasiado pequeño")
+        else:
+            print("Demasiado pequeño")
 
-    if numero_aleatorio==numero_usuario:
-        print("Has ganado")
-
-
-
-
+        if numero_aleatorio==numero_usuario:
+            print("Has ganado")
