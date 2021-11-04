2142059b202a563712d2ffd6425aa9ea819a4626
maariagarrcia <mrrmn@myuax.com>
2021-11-04 15:40:58 +0100

    guardar4

--- juego.py
+++ juego.py
@@ -1,37 +1,31 @@
 import random
-
 #El juego consiste en que el ORDENADOR escoge un número al azar (random)del 0 al 99.
 # El jugador tiene que adivinarlo con el  menor número de intentos posible.
-# Cada ves que el jugador se equivoca el ORDENADOR le tiene que dar una pista,  indicando
+# Cada ves que el jugador se equivoca el ORDENADOR le tiene que dar una pista, indicando
 # si el número escogido inicialmente es menor o mayoor que el propuest por el JUGADOR.
  
-numero_aleatorio=random.randint(min,max)
 min=0
 max=99
-numero_aleatorio= int
 
-print("Soy el ordenador y voy a pensar un número entre" +str(min) +"y"str(max))
+
+print("Soy el ordenador y voy a pensar un número entre" + str(min) + "y" str(max))
 print("Ya lo tengo, ahora tienes que adivinar mi numero secreto")
 print("Si te equivocas te doy pistas :) ")
 
-while True:
-    if 0<numero_aleatorio<99:
-        print("Numero entre los valores 0 y 99")
-    break
+encontrado= False
+intento=0
+numero_secreto=random.randint(0,100)
+print("He pensado en el", numero_secreto)
 
-while True:
-    numero_usuario= input()
-    if 0<=numero_usuario<=99:
-        print(numero_usuario)
-    else:
-        pass
-    break
-
-
-while numero_aleatorio==numero_usuario:
-    if numero_aleatorio>numero_usuario:
-        print("Demasiado grande")
-    else:
-        print("Demasiado pequeño")
-    if numero_aleatorio==numero_usuario:
+while not encontrado:
+    intento= intento+1
+    numero_usuario= int(input("Dime, ¿cual es el numero secreto?"))
+    encontrado=(numero_secreto==numero_usuario)
+    print(intento+str(intento)+'>' 'end'=)
+    
+    if(encontrado):
         print("Has ganado")
+    elif(numero_secreto>numero_usuario):
+        print("Demasiado grande. El "+str("numero_usuario")"+ es mayor que numero secreto"")
+    else:
+        print("Demasiado pequeño. El +str("numero_usuario") "+ es menor que numero secreto")
