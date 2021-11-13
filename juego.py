b330df0e8aec928ac58d1211ec13c203639d788b
maariagarrcia <mrrmn@myuax.com>
2021-11-13 17:11:48 +0100

    fase 3

--- juego.py
+++ juego.py
@@ -27,16 +27,16 @@ def clear():
 #           False---> No cumple requisitos
 def input_usuarioOk(input, min, max):
     ok=False
+
     try:
               numero_usuario=int(input("Dime, ¿cual es el numero secreto?"))
     except:
         print("Solo acepto numeros. Vuelve a intentarlo")
-        pass
+        pass #No es un numero
     else:
         if(min<=numero_usuario<=max):
                 ok= True
-        else:
-            print("Solo acepto numeros entre " + str(min) + " y " + str(max))
+        
     return ok
 
 def aceptar_opcion_menu():
@@ -48,14 +48,14 @@ def aceptar_opcion_menu():
           
         if (inputusuario== "F" or inputusuario == "f"):
              # Finalizar
-            opcion=1
+            opcion=-1
             salir= True
-        elif (inputusuario== "p" or inputusuario== "p"):
+        elif (inputusuario== " P" or inputusuario== "p"):
             # Ver puntuaciones
-            opcion=2
+            opcion=-2
         elif input_usuarioOk(inputusuario, 1,4):
-                #JUGAR CON NIVEL ENTRE 1,4
-                salir= True
+            #JUGAR CON NIVEL ENTRE 1,4
+            salir= True
         else:
             # Opcion incorrecta
             print(Fore.RED + "*ATENCION: Seleccione una opcion valida" + Fore.WHITE )
@@ -88,7 +88,7 @@ def aceptarjugada(min, max):
 
     while (not salir):
         inputusuario= input(Fore.YELLOW+ "Dime cual crees que es el numero secreto")
-        if(inputusuario == "F" or inputusuario == "F"):
+        if(inputusuario == "F" or inputusuario == "f"):
             jugada:-1
             salir: True
         elif (inputusuario=="A" or inputusuario== "a"):
@@ -112,8 +112,7 @@ def aceptarjugada(min, max):
     return  jugada
 
 
-def mostrarayuda():
-    pass
+
 
 def avisariniciopartida(min, max, numero_secreto, depurar):
     if (depurar):
@@ -128,7 +127,7 @@ def avisariniciopartida(min, max, numero_secreto, depurar):
     print(Fore.RED + "Durante el juego puedes obtener ayuda pulsando A" + Fore.Yellow)
     print("Empieza  la partida")
 
-
+    
 def avisarfinpartida(numero_secreto, intento, maxintentos, encontrado, finalizar):
     print(Fore.YELLOW+ "El juego  ha finalizado :(")
     print()
@@ -144,18 +143,20 @@ def avisarfinpartida(numero_secreto, intento, maxintentos, encontrado, finalizar
     print("El numero que había pensado es el " + str(numero_secreto))
     print("Has realizad" + str(intento) + "intentos")
 
-def mostrarpista(minactual, maxactual):
+def mostrarayuda(minactual, maxactual):
     print(
         Fore.RED + "*Pista: El numero secreto debe estar entre" +
-        Fore.White +str(minactual) +
+        Fore.WHITE +str(minactual) +
         Fore.RED + "y" +
-        Fore.White + str (maxactual)
+        Fore.WHITE + str (maxactual)
     )
+
 # Donde se guardan las listas
 def actualizarPuntuaciones(diccionarioPuntuaciones):
     print(Fore.WHITE)
     print("Actualizacion de puntuaciones")
     nombreJugador= input("Dime tu nombre")
+
     if(nombreJugador in diccionarioPuntuaciones):
         #Si el jugador existe en el diccionario apuntar una partida mas
         diccionarioPuntuaciones [nombreJugador] = diccionarioPuntuaciones[nombreJugador] + 1
@@ -164,11 +165,16 @@ def actualizarPuntuaciones(diccionarioPuntuaciones):
         diccionarioPuntuaciones[nombreJugador] =1
     
 def mostrarPuntuaciones (diccionarioPuntuaciones):
-    print(Fore. WHITE)
+    print(Fore.WHITE)
     print("Lista de partidas ganadas")
     print(diccionarioPuntuaciones)
 
-def jugar(min,max, maxIntentos, puntuaciones):
+def aceptarJugadaMasterIA(minActual, maxActual):
+    jugadaIA=int((maxActual+minActual)/2)
+    print(jugadaIA)
+    return jugadaIA
+
+def jugar(min,max, maxIntentos, puntuaciones, usarIA):
     clear()
 
     numero_secreto=random.randint(min,max)
@@ -180,15 +186,19 @@ def jugar(min,max, maxIntentos, puntuaciones):
     maxActual=  max    #Es el maxim que ya sabemos que puede ser el secreto
 
     while (not finalizar and not encontrado and intento < maxIntentos):
-        numero_usuario= aceptarjugada(min,max)
+        if (usarIA):
+            # mostrarAyuda(minActual, maxActual)
+            numero_usuario=aceptarJugadaMasterIA(minActual, maxActual)
+        else:
+            numero_usuario= aceptarjugada(min,max)
         if(numero_usuario == -1):
             finalizar= True
         elif (numero_usuario == -2):
-            mostrarpista(minActual, maxActual)
+            mostrarayuda(minActual, maxActual)
         else:
-            intento= intento+1
+            intento= intento +1
             #Comprobar si el jugadoor ha acertado el numero
-            encontrado=(numero_usuario==numero_secreto)
+            encontrado= (numero_usuario==numero_secreto)
             if(numero_usuario>numero_secreto):
                 maxActual=numero_usuario
                 print(Fore.YELLOW +  "El"  +str(numero_usuario)+ " es MAYOR que el numero secreto")
@@ -201,7 +211,7 @@ def jugar(min,max, maxIntentos, puntuaciones):
     if(encontrado):
         actualizarPuntuaciones(diccionarioPuntuaciones)
 
-## Inicio PROGRAMA
+# INICIO PROGRAMA
 
 diccionarioPuntuaciones= {}
 
@@ -225,6 +235,8 @@ while (not finalizar):
 
 print(Fore.GREEN  + "Nos vemos otro dia :)")
 print(Fore.WHITE)
+
+
 
 
 
